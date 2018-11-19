ddba00acc0737becff2efbbeabc7d3da1de53278
eddie brett <eddiebrett@hotmail.co.uk>
2018-11-19 18:49:46 +0000

    sendEmail.js form

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -67,6 +67,10 @@ function fetchGitHubInformation(event) {
                     $("#gh-user-data").html(
                         `<h2>No info found for user ${username}</h2>`);
                 }
+                else if(errorResponse.status === 403) {
+                    var resetTime = new Date (errorResponse.getResponseHeader("X-RateLimit-Reset")*1000);
+                    $("#gh-user-data").html(`<h4>Too many requests, pleaser wait until ${resetTime.toLocaleTimeString()}</h4>`);
+                }
                 else {
                     console.log(errorResponse);
                     $("#gh-user-data").html(
